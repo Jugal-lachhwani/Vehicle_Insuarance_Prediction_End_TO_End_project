fae1de808bdd5d9bb3e04b250433ed5b6a0f9792
Jugal-lachhwani <jugallachhwani15@gmail.com>
2025-10-14 14:07:35 +0530

    Added code for Data Ingestion

--- demo.py
+++ demo.py
@@ -1,12 +1,25 @@
-from src.logger import logging
+# from src.logger import logging
 
-# logging.debug('This is debug msg')
+# # logging.debug('This is debug msg')
 
-from src.exception import MyException
-import sys
+# from src.exception import MyException
+# import sys
 
-try:
-    a = 1+'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+# try:
+#     a = 1+'Z'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
+
+from src.pipline.training_pipeline import TrainPipeline
+
+pipline = TrainPipeline()
+pipline.run_pipeline()
+
+# from src.constants import *
+
+# print(MONGODB_URL_KEY)
+
+# import os
+# CONNECTION_URL = os.getenv("MONGODB_URL")
+# print(CONNECTION_URL)
